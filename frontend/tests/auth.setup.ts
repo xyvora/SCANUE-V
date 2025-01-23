b8ede6a281145c094a21c85a1152dede5d7e02dd
Paul Sanders <paul@paulsanders.dev>
2025-01-22 21:02:34 -0500

    Add debug logging

--- frontend/tests/auth.setup.ts
+++ frontend/tests/auth.setup.ts
@@ -5,17 +5,10 @@ const authFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
   await page.goto("/login");
+  console.log(firstSuperuserEmail);
   await page.getByPlaceholder("Email").fill(firstSuperuserEmail);
   await page.getByPlaceholder("Password").fill(firstSuperuserPassword);
   await page.getByRole("button", { name: "Log In" }).click();
-  console.log("Clicked login button, waiting for navigation...");
-  // await page.waitForURL("/");
-  try {
-    await page.waitForURL("/", { timeout: 30000 });
-  } catch (error) {
-    console.error("Navigation to '/' failed:", error);
-    const body = await page.content();
-    console.error("Page content:", body);
-  }
+  await page.waitForURL("/");
   await page.context().storageState({ path: authFile });
 });
