ada22b6b02581a450ec3d227ea3a950b308caa23
Paul Sanders <paul@paulsanders.dev>
2025-01-27 14:27:49 -0500

    Fix import name

--- frontend/src/app/account/update-password/page.tsx
+++ frontend/src/app/account/update-password/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import UpdatePasswordForm from "@/components/UpdatePassword";
+import UpdatePasswordForm from "@/components/UpdatePasswordForm";
 
 export const metadata: Metadata = {
   title: "SCANUE-V - Update Password",
