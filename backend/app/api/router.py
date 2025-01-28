27f68fce66e1f448dc213890bf00a24a55f1583f
Paul Sanders <paul@paulsanders.dev>
2025-01-28 08:32:27 -0500

    Change backend route from scan to chat

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,8 @@
-from app.api.routes import health, login, scan, users
+from app.api.routes import chat, health, login, users
 from app.core.utils import APIRouter
 
 api_router = APIRouter()
+api_router.include_router(chat.router)
 api_router.include_router(health.router)
 api_router.include_router(login.router)
-api_router.include_router(scan.router)
 api_router.include_router(users.router)
